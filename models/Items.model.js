9248d6d4081116c8c4d5e288ae2ca9297bfcaa22
youssef salim <salimgithub@gmail.com>
2021-01-29 18:00:41 +0000

    test gitpod

--- models/Items.model.js
+++ models/Items.model.js
@@ -20,4 +20,18 @@ const ItemSchema = new Schema({
   }
 })
 
+module.exports = Item = mongoose.model('Item', ItemSchema)const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+//* create a Item Scheme
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 module.exports = Item = mongoose.model('Item', ItemSchema)
